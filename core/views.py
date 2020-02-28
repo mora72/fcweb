7446d8a3f0811445fc78cc5f3eaa34f5251d177b
Carlos H Mora Jr <carlosmorajunior@gmail.com>
2020-02-27 21:17:10 -0300

    2o Commit

--- core/views.py
+++ core/views.py
@@ -1,5 +1,27 @@
 from django.shortcuts import render
+from sys import path
+from core.arquivos import *
+from datetime import date
 
 
 def main_dash(request):
-    return render(request, 'core/maindash.html')
+    path.append('C:/Users/carlo/PycharmProjects/fc)')
+    anoatual = date.today().year
+    mesatual = date.today().month
+    anotrabalho = anoatual
+    mestrabalho = mesatual
+
+    arqlistameiossaldo = Arquivolista('/Users/carlo/PycharmProjects/fc/basemeiossaldo.pck1', 'MeiosSaldo')
+    listameiossaldo = arqlistameiossaldo.ler()
+
+    arqlistameios = Arquivolista('/Users/carlo/PycharmProjects/fc/basemeios.pck1', 'Meios')
+    listameios = arqlistameios.ler()
+
+    lista_saldo = []
+    for x in listameiossaldo:
+        if x["mes"] == mestrabalho and x["ano"] == anotrabalho:
+            nomemeio = list(filter(lambda meio: meio["cod"] == x["cod"], listameios))[0]["nome"]
+            registro = x
+            registro['nomemeio'] = nomemeio
+            lista_saldo.append(registro.copy())
+    return render(request, 'core/maindash.html', {'listasaldo': lista_saldo})
